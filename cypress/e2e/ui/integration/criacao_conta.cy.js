fc3e9791a3a76bc9ebde04702788f836d3049b41
Victor <vferreirap2@gmail.com>
2023-06-25 22:25:38 -0300

    Add env vars file

--- cypress/e2e/ui/integration/criacao_conta.cy.js
+++ cypress/e2e/ui/integration/criacao_conta.cy.js
@@ -1,10 +1,12 @@
 const minha_conta = require('../../../support/pages/minha_conta.page')
 const data = require('../../../fixtures/perfil.json')
+const env = require('../../../fixtures/env_vars.json')
 
 describe('Cria uma nova conta na loja da Ebac', () => {
 
-  it('Cria uma nova conta com sucesso usando page objects', () => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  it.only('Cria uma nova conta com sucesso usando page objects', () => {
+    
+    cy.visit(`${env.HOST}minha-conta/`)
     minha_conta.myAccountPage.fill_register_form()
     minha_conta.myAccountPage.fill_name_details_form()
     cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
