9c31a97191bb372ea78429317f64d91dbf986e82
vpereira <vpereira@agmednet.com>
2023-11-10 23:36:26 -0300

    Add git page actions Update files

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'p728fh',
   e2e: {
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
     reporter: 'mochawesome',
